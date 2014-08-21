17ad368a3b7322016dccfec9494f1e6baf912433
Astha <sweetastha23@gmail.com>
2014-08-21 23:48:50 +0530

    cleaning up

--- basic_recommenders.py
+++ basic_recommenders.py
@@ -3,6 +3,10 @@ import nltk
 import operator
 from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
 import copy
+import sys
+
+from common_functions import *
+from naive_bayes import *
 # from nltk.stem.snowball import SnowballStemmer
 # stemmer = SnowballStemmer("english")
 
@@ -10,118 +14,11 @@ import copy
 processedHashTags = []
 processedTweets = []
 
-
 stopWords=[]
 stopWordListFileName="stopwords.txt"
 
-def getStopWords():
-	stopWords = []
-	stopWords.append("at_user")
-	stopWords.append("url")
-
-	fp = open(stopWordListFileName, 'r')
-	line = fp.readline()
-	while line:
-		word = line.strip()
-		stopWords.append(word)
-		line = fp.readline()
-	fp.close()
-	return stopWords
-
-def replaceThreeOrMore(word):
-	pattern = re.compile(r"(.)\1\1+", re.DOTALL) #re.DOTALL means . represents any character including '\n'
-	return pattern.sub(r"\1", word)
-
-def ContainsHashTag(tweet):
-	val = re.search(r".*(#\w+)",tweet)
-	if(val is None):
-		return False
-	else:
-		return True
-
-def retrieveHashTags(tweet):
-	pattern=re.compile(r"#[\w_-]+")
-	return [ h.lower() for h in pattern.findall(tweet)]
-
-
-def processTweet(tweet):
-	#Convert @username to ""
-	tweet = re.sub('@[\S]+','',tweet)
-
-	if(not ContainsHashTag(tweet)):
-		return ""
-	#Convert www.* or https?://* to URL
-	tweet = re.sub('((www\.[\S]+)|(https?://[\S]+))','URL',tweet)
-	
-	#trim the appearing punctuations from begin and end of tweet
-	tweet = tweet.strip('\'"?.,!')
-	# remove punctuation from tweet
-	tweet = re.sub('[?,."!]','',tweet)
-	#Remove additional white spaces
-	tweet = re.sub('[\s]+', ' ', tweet)
-	return tweet
-
 slangDictFileName = "slangDict.txt"
 slangDict={}
-def readSlangDictionary():
-	slangDict={}
-	f=open(slangDictFileName,'r')
-	lines = f.readlines()
-	for line in lines:
-		arr=line.split(":")
-		slangDict[arr[0].strip()]=arr[1].strip()
-	f.close()
-	return slangDict
-
-def getFeatures(tweet):
-	featureVector = []
-	words = tweet.split()
-	for w in words:
-
-		arr=w.split('#')
-		if(arr[0]!=""):
-			#replace two or more with one occurrence
-			w = replaceThreeOrMore(arr[0])
-			#strip punctuation
-			w = w.strip('\'"?,.-')
-			#the word should start with alphabet or it should be a hashtag
-			val = re.search(r"(^[#a-zA-Z])", w)
-			w = w.lower()
-
-			#ignore if it is a stop word
-			if(w in stopWords or val is None):
-				continue
-			else:
-				# if you want to add stemming, uncomment line below
-				# w = stemmer.stem(w)
-				if w in slangDict.keys():
-					replacementWord = slangDict[w]
-					for word in replacementWord.split():
-						if not word.lower() in stopWords:
-							featureVector.append(word.lower())
-				else:
-					featureVector.append(w)
-	return featureVector
-
-def getVocabulary(features):
-	vocabulary = dict()
-	vocabCount = 0
-	for feature in features:
-		for word in feature:
-			if not word in vocabulary.keys():
-				vocabulary[word] = vocabCount
-				vocabCount += 1
-	return vocabulary
-
-def getWordFrequency(features):
-	wordFreqMap = dict()
-	for feature in features:
-		for word in feature:
-			if wordFreqMap.has_key(word):
-				wordFreqMap[word] += 1
-			else:
-				wordFreqMap[word] = 1
-	return wordFreqMap
 
 # presence feature vector
 def createPresenceFeatureVectors(features):
@@ -166,7 +63,8 @@ def initialize():
 	slangDict = readSlangDictionary()
 
 tarr = []
-fp = open("tweets", 'r')
+filename = sys.argv[1]
+fp = open(filename, 'r')
 line = fp.readline()
 while line:
 	tarr.append(line)
@@ -194,18 +92,6 @@ def preProcessAllTweets(tweetArray, hashtagFileName, wordsFileName):
 		processedTweets.append(feature)	
 	hashtagFile.close()
 	wordsFile.close()
-
-def calculateHashtagFrequency(processedHashTags):
-	hashtagFreqMap = {}
-	for hashtagList in processedHashTags:
-		for hashEle in hashtagList:
-			if hashtagFreqMap.has_key(hashEle):
-				hashtagFreqMap[hashEle] += 1
-			else:
-				hashtagFreqMap[hashEle] = 1
-	return hashtagFreqMap
-
-
 
 def test(processedHashTags, processedTweets, testTweets):
 	featureVectors, vocabulary = createTFIDFFeatureVectors(processedTweets)
@@ -237,6 +123,9 @@ def test(processedHashTags, processedTweets, testTweets):
 		print finalTags
 		break
 
+
+# Ranking methods 
+
 def tweetScoreBased(closenessScores, processedHashTags):
 	relevantTweets = sorted(closenessScores.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]
 	# print relevantTweets
@@ -250,8 +139,6 @@ def tweetScoreBased(closenessScores, processedHashTags):
 			tempHashtagSet.add(tag)
 			finalTags.append(tag)
 	return finalTags[:5]
-
-
 
 def localFrequencyRanking(relevantTags, k):
 	freqMap = {}
@@ -269,114 +156,6 @@ def globalFrequencyRanking(relevantTags, hashtagFreqMap, k):
 		freqMap[tag] = hashtagFreqMap[tag]
 	sortedTags = sorted(freqMap.iteritems(), key=operator.itemgetter(1), reverse=True)[:k]
 	return [ tag for tag,freq in sortedTags]
-
-
-
-def createNaiveBayesFeatureVector(feature, featureVector):
-	for word in feature:
-		featureVector[word] = 1
-	return featureVector
-
-
-def naiveBayesClassifier(processedHashTags, processedTweets, testTweets):
-	vocabulary = getVocabulary(processedTweets)
-	for key,value in vocabulary.items():
-		vocabulary[key] = 0
-
-	train = []
-	test = []
-
-	for i in range(len(processedTweets)):
-		feature = processedTweets[i]
-		featureVector = copy.deepcopy(vocabulary)
-		featureVector = createNaiveBayesFeatureVector(feature, featureVector)
-		for hashtag in processedHashTags[i]:
-			train.append((featureVector,hashtag))
-
-	print "Trainig shuru"
-	classifier = nltk.classify.NaiveBayesClassifier.train(train)
-	print "Training Ends \n"
-
-	for feature in testTweets:
-		featureVector = copy.deepcopy(vocabulary)
-		featureVector = createNaiveBayesFeatureVector(feature, featureVector)
-		test.append((featureVector))
-
-	
-	for i in range(len(testTweets)):
-		tweet = testTweets[i]
-		pdist = classifier.prob_classify(test[i])
-
-		allHashTags = pdist.samples()
-		hashtagProb = {}
-		for ht in allHashTags:
-			hashtagProb[ht] = pdist.prob(ht)
-		sortedProbs = sorted(hashtagProb.iteritems(), key=operator.itemgetter(1))
-
-		bestMatch = reversed(sortedProbs[-5:])
-		bestMatch = [i[0] for i in bestMatch]
-		print tweet
-		print bestMatch
-		print sortedProbs[-5:]
-		
-		print "\n--------------------\n"
-
-def hashTagMaps(processedTweets, processedHashTags):
-	hashtagFreqMap = calculateHashtagFrequency(processedHashTags)
-	hashtagToWordFreq = {}
-	vocabulary = getVocabulary(processedTweets)
-	vocabularyCount = len(vocabulary.keys())
-	for i in range(len(processedHashTags)):
-		hashtagList = processedHashTags[i]
-		for hashtag in hashtagList:
-			if not hashtagToWordFreq.has_key(hashtag):
-				hashtagToWordFreq[hashtag] = [0]*vocabularyCount
-			for word in set(processedTweets[i]):
-				hashtagToWordFreq[hashtag][vocabulary[word]] += 1
-	return hashtagFreqMap, hashtagToWordFreq
-
-def naiveBayesRecommender(processedTweets, processedHashTags, testTweets, k):
-	hashtagFreqMap, hashtagToWordFreq = hashTagMaps(processedTweets, processedHashTags)
-	wordFreqMap = getWordFrequency(processedTweets)
-	vocabulary = getVocabulary(processedTweets)
-	vocabSize = len(vocabulary.keys())
-	totalFreq = sum(hashtagFreqMap.values())
-	totalVocabFreq = sum(wordFreqMap.values())
-	smoothingFactor = 0.001
-	vocabSet = set(vocabulary.keys())
-
-
-	vocabulary = getVocabulary(processedTweets)
-	for testFeature in testTweets:
-		featureVector = [0]*vocabSize
-		hashtagProbMap = {}
-		restWords = vocabSet - set(testFeature)
-		for tag in hashtagFreqMap.keys():
-			hashtagProbMap[tag] = float(hashtagFreqMap[tag])/totalFreq #Prior Probability
-			for word in testFeature:
-				if hashtagProbMap[tag] ==  0:
-					print word
-					exit(0)
-				if vocabulary.has_key(word):
-					freq = hashtagToWordFreq[tag][vocabulary[word]]
-					if freq != 0:
-						hashtagProbMap[tag] *= float(freq) / hashtagFreqMap[tag]
-					else:
-						hashtagProbMap[tag] *=  float(wordFreqMap[word]) / totalVocabFreq
-			
-			for word in restWords:
-				freq = hashtagToWordFreq[tag][vocabulary[word]]
-				if freq / hashtagFreqMap[tag] == 1:
-					hashtagProbMap[tag] *= smoothingFactor
-				else:
-					hashtagProbMap[tag] *= (1 - (float(freq) / hashtagFreqMap[tag]))
-
-		topKpairs = sorted(hashtagProbMap.iteritems(), key=operator.itemgetter(1))[-k:]
-		finalTags = [tag for tag,prob in reversed(topKpairs)]
-		print testFeature
-		print [(tag,str(prob)) for tag,prob in reversed(topKpairs)]
-		print "-------------------\n"
-
 
 def fiveFoldValidation():
 	preProcessAllTweets(tarr,"h.txt","w.txt") #sets the processedHashTags and processedTweets
---
+++ naive_bayes.py
@@ -0,0 +1,99 @@
+
+from common_functions import *
+
+# Naive Bayes Self Implementation
+
+def naiveBayesRecommender(processedTweets, processedHashTags, testTweets, k):
+  hashtagFreqMap, hashtagToWordFreq = hashTagMaps(processedTweets, processedHashTags)
+  wordFreqMap = getWordFrequency(processedTweets)
+  vocabulary = getVocabulary(processedTweets)
+  vocabSize = len(vocabulary.keys())
+  totalFreq = sum(hashtagFreqMap.values())
+  totalVocabFreq = sum(wordFreqMap.values())
+  smoothingFactor = 0.001
+  vocabSet = set(vocabulary.keys())
+
+
+  vocabulary = getVocabulary(processedTweets)
+  for testFeature in testTweets:
+    featureVector = [0]*vocabSize
+    hashtagProbMap = {}
+    restWords = vocabSet - set(testFeature)
+    for tag in hashtagFreqMap.keys():
+      hashtagProbMap[tag] = float(hashtagFreqMap[tag])/totalFreq #Prior Probability
+      for word in testFeature:
+        if hashtagProbMap[tag] ==  0:
+          print word
+          exit(0)
+        if vocabulary.has_key(word):
+          freq = hashtagToWordFreq[tag][vocabulary[word]]
+          if freq != 0:
+            hashtagProbMap[tag] *= float(freq) / hashtagFreqMap[tag]
+          else:
+            hashtagProbMap[tag] *=  float(wordFreqMap[word]) / totalVocabFreq
+      
+      for word in restWords:
+        freq = hashtagToWordFreq[tag][vocabulary[word]]
+        if freq / hashtagFreqMap[tag] == 1:
+          hashtagProbMap[tag] *= smoothingFactor
+        else:
+          hashtagProbMap[tag] *= (1 - (float(freq) / hashtagFreqMap[tag]))
+
+    topKpairs = sorted(hashtagProbMap.iteritems(), key=operator.itemgetter(1))[-k:]
+    finalTags = [tag for tag,prob in reversed(topKpairs)]
+    print testFeature
+    print [(tag,str(prob)) for tag,prob in reversed(topKpairs)]
+    print "-------------------\n"
+
+
+
+
+# NLTK Naive Bayes 
+
+def createNaiveBayesFeatureVector(feature, featureVector):
+  for word in feature:
+    featureVector[word] = 1
+  return featureVector
+
+def naiveBayesClassifier(processedHashTags, processedTweets, testTweets):
+  vocabulary = getVocabulary(processedTweets)
+  for key,value in vocabulary.items():
+    vocabulary[key] = 0
+
+  train = []
+  test = []
+
+  for i in range(len(processedTweets)):
+    feature = processedTweets[i]
+    featureVector = copy.deepcopy(vocabulary)
+    featureVector = createNaiveBayesFeatureVector(feature, featureVector)
+    for hashtag in processedHashTags[i]:
+      train.append((featureVector,hashtag))
+
+  print "Trainig shuru"
+  classifier = nltk.classify.NaiveBayesClassifier.train(train)
+  print "Training Ends \n"
+
+  for feature in testTweets:
+    featureVector = copy.deepcopy(vocabulary)
+    featureVector = createNaiveBayesFeatureVector(feature, featureVector)
+    test.append((featureVector))
+
+  
+  for i in range(len(testTweets)):
+    tweet = testTweets[i]
+    pdist = classifier.prob_classify(test[i])
+
+    allHashTags = pdist.samples()
+    hashtagProb = {}
+    for ht in allHashTags:
+      hashtagProb[ht] = pdist.prob(ht)
+    sortedProbs = sorted(hashtagProb.iteritems(), key=operator.itemgetter(1))
+
+    bestMatch = reversed(sortedProbs[-5:])
+    bestMatch = [i[0] for i in bestMatch]
+    print tweet
+    print bestMatch
+    print sortedProbs[-5:]
+    
+    print "\n--------------------\n"
